b783f4dd86ef0b5c4611cd517771615c630bf9f1
takubokudori <takubokudori@gmail.com>
2018-07-04 22:27:25 +0900

    fix typo&install.bat

--- install.bat
+++ install.bat
@@ -1,5 +1,14 @@
-@echo off
 
+where /Q git
+if %ERRORLEVEL% neq 0 goto GIT_NOT_INSTALLED
 mkdir %HOME%\vim-backup
-cp .vimrc %HOME%\_vimrc
-cp .gvimrc %HOME%\_gvimrc
+copy .vimrc %HOME%\_vimrc
+copy .gvimrc %HOME%\_gvimrc
+
+git clone https://github.com/Shougo/dein.vim C:\vim\dein\repos\github.com\Shougo\dein.vim
+
+:GIT_NOT_INSTALLED
+echo "install git!!"
+goto END
+
+:END
---
+++ install2.bat
@@ -0,0 +1,3 @@
+rem does not work
+move %HOME%\vimrcs\myvim\vimproc_win64.dll C:\vim\dein\repos\github.com\Shougo\vimproc\lib
+move %HOME%\vimrcs\myvim\colorschemes\* %VIMHOME%\vim81\colors
