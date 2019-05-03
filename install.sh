446ea6881abd38d1da64e292b69b237cef3aa4b9
takubokudori <takubokudori@gmail.com>
2019-05-03 23:31:32 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+if !(type "git" > /dev/null 2>&1); then
+	echo "Install git!!"
+	echo "sudo apt install git"
+	return
+fi
+
+if !(type "ctags" > /dev/null 2>&1); then
+	echo "Install ctags!!"
+	echo "sudo apt install ctags"
+	return
+fi
+
+if !(type "gtags" > /dev/null 2>&1); then
+	echo "Install gtags!!"
+	echo "sudo apt install gtags"
+	return
+fi
 mkdir ~/vim-backup
 cp ./.vimrc ~
 cp ./.gvimrc ~
