f66a8703ca520103a4c0a954303ded37220ff8ba
takubokudori <takubokudori@gmail.com>
2020-07-12 16:35:49 +0900

    update sh

--- install.sh
+++ install.sh
@@ -1,26 +1,24 @@
 #!/bin/sh
 
 if !(type "git" > /dev/null 2>&1); then
-	echo "Install git!!"
+	echo "[Error] git is not intalled!!"
 	echo "sudo apt install git"
 	return
 fi
 
 if !(type "ctags" > /dev/null 2>&1); then
-	echo "Install ctags!!"
-	echo "sudo apt install ctags"
-	return
+	echo "[Warning] ctags is not installed."
+	echo "sudo apt install exuberant-ctags"
 fi
 
 if !(type "gtags" > /dev/null 2>&1); then
-	echo "Install gtags!!"
-	echo "wget https://ftp.gnu.org/pub/gnu/global/global-6.6.3.tar.gz -O gtags.tar.gz"
-	echo "tar zxvf gtags.tar.gz"
-	echo "cd gtags"
+	echo "[Warning] gtags is not installed."
+	echo "wget https://ftp.gnu.org/pub/gnu/global/global-6.6.4.tar.gz -O global-6.6.4.tar.gz"
+	echo "tar zxvf global-6.6.4.tar.gz"
+	echo "cd global-6.6.4"
 	echo "./configure"
 	echo "make"
 	echo "make install"
-	return
 fi
 
 mkdir ~/vim-backup
@@ -30,3 +28,4 @@ cp ./.vsvimrc ~
 cp ./.vrapperrc ~
 cp ./ideavimrc/init/init.vim ~/.ideavimrc
 cp ./myvim/snippets/* ~/.vim/dein/repos/github.com/Shougo/neosnippet-snippets/neosnippets/
+
